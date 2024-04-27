ea95029081823c90f7b9916cdb72216f0db934f8
AbanoubGamalll <abanoub.gamal.fekry@gmail.com>
2024-04-28 00:48:59 +0300

    update README.md

--- Paths.py
+++ Paths.py
@@ -1,14 +1,5 @@
 # DataSet
-# - EchoNet-Dynamic
-# ----- FileList.csv
-# ----- VolumeTracings.csv
-# ----- Videos
-# ---------- 0X1A0A263B22CCD966.avi
-# ---------- 0X1A2A76BDB5B98BED.avi
-# ---------- 0X1A2C60147AF9FDAE.avi
-# ---------- etc.
-
-_dataRootPath = "E:/Fcis Material/Graduation Project/DataSet (EchoNet-Dynamic)/"
+_dataRootPath = ""
 _videosPath = _dataRootPath + 'Videos'
 _fileNamesPath = _dataRootPath + 'FileList.csv'
 _volumeTracingPath = _dataRootPath + 'VolumeTracings.csv'
