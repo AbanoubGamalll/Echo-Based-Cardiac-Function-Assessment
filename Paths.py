afb1912a59ed1f595cca31a839d77b2fa475e348
AbanoubGamalll <abanoub.gamal.fekry@gmail.com>
2024-04-29 14:28:17 +0300

    Edit Paths

--- Paths.py
+++ Paths.py
@@ -1,16 +1,16 @@
 # DataSet
-_dataRootPath = 'E:/Fcis Material/Graduation Project/DataSet (EchoNet-Dynamic)'
+_dataRootPath = ''
 _videosPath = _dataRootPath + '/Videos'
 _fileNamesPath = _dataRootPath + '/FileList.csv'
 _volumeTracingPath = _dataRootPath + '/VolumeTracings.csv'
 
 # Loaded Videos
-_loadedVideosPath = './Loaded Videos'
+_loadedVideosPath = ''
 
 # Transformer
-_transformerModelPath = './Models/TransformerModel.pt'
+_transformerModelPath = 'TransformerModel.pt'
 
 # U-NET
 _trueMasksPath = './True Masks'
-_ED_Model_Path = 'Models/ED_U_NET_Model.weights.h5'
-_ES_Model_Path = './Models/ES_U_NET_Model.weights.h5'
+_ED_Model_Path = 'ED_Unet.weights.h5'
+_ES_Model_Path = 'ES_Unet.weights.h5'
