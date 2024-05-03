540678c5ad616b486b4ca6c1d13f1a29e2f9bbe4
AbanoubGamalll <abanoub.gamal.fekry@gmail.com>
2024-05-03 10:17:33 +0300

    Edit API File

--- Api.py
+++ Api.py
@@ -21,7 +21,8 @@ ES_Model = loadUnetModel(_ES_Model_Path)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {}
+
 
 @app.post("/ef")
 def predict_EF(file: UploadFile = File(...)):
@@ -30,21 +31,23 @@ def predict_EF(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
 
     # Predict
-    ES_Frame_IMG, ED_Frame_IMG = Detect_ESED_Frame(file.filename, transformerModel)
-
-    if ES_Frame_IMG is None or ED_Frame_IMG is None:
-        return {"error": "Unable to open video file"}
-
-    efPred = predictLVForEDESFrames(ES_Frame_IMG, ED_Frame_IMG, ED_Model, ES_Model)
+    # ES_Frame_IMG, ED_Frame_IMG = Detect_ESED_Frame(file.filename, transformerModel)
+    # if ES_Frame_IMG is None or ED_Frame_IMG is None:
+    #     return {"error": "Unable to open video file"}
+    #
+    # efPred = predictLVForEDESFrames(ES_Frame_IMG, ED_Frame_IMG, ED_Model, ES_Model)
 
     # Remove Video
     os.remove(file.filename)
+    efPred = 55.66
+    print("fileName", file.filename)
+    print("efPredicted", efPred)
 
     return {
-        "filename": file.filename,
-        "EF Predicted": efPred
+        "fileName": file.filename,
+        "efPredicted": efPred
     }
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
